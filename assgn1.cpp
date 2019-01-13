1200a04e152306f4be67f863842f9aeb2bc47d6d
amitkvikram <amitkvikram@gmail.com>
2019-01-13 10:28:36 +0530

    assgn1 updated

--- assgn1.cpp
+++ assgn1.cpp
@@ -6,8 +6,8 @@ using namespace std;
 const int inf = 1e8;
 
 typedef struct{
-    int haredist;
-    int turtleDist;
+    int hareTime;
+    int turtleTime;
 } result;
 
 typedef struct {
@@ -40,7 +40,7 @@ int main(){
     //Pipes Creation
     if( pipe(hareToTurtle) < 0  || 
         pipe(turtleToHare) < 0  ||
-        pipe(godtoTurtle) < 0   ||
+        pipe(godToTurtle) < 0   ||
         pipe(godToHare) < 0     ||
         pipe(HareToReporter) < 0||
         pipe(hareToMaster) < 0    ){
@@ -66,68 +66,52 @@ int main(){
         cout<<"Parent Process: "<<"harePid turtlePid godPid reporterPid\n";
         cout<<"Parent Process: "<<harePid<<" "<<turtlePid<<" "<<godPid<<" "<<reporterPid<<endl;
         int hareTime = -1, turtleTime = -1;
-        while(true){
-            if(hareTime == -1) read(masterHare[0], &hareTime, sizeof(hareTime));
-            if(turtleTime == -1) read(masterTurtle[0], &turtleTime, sizeof(turtleTime));
-            if(hareRunning && hareTime > 0){
-                hareRunning = false;
-            }
-            if(turtleRunning && turtleTime > 0){
-                turtleRunning = false;
-            }
-
-            if(!hareRunning && !turtleRunning){
-                break;
-            }
-        }
-        int status;
-        kill(godPid, SIGTERM);
-        kill(reporterPid, SIGTERM);
-        /*Wait for Child Process*/
-        for(int i = 0; i < 4; i ++){
-            waitpid(pids[i], &status, 0);
-        }
-
     }
 
     else if(harePid == 0){   //Hare Process
-        cout<<"HareProces: "<<getpid()<<endl;  
-        int hareDist = 0, hareTime = 0, turtleTime = -1, turtleDist = 0;
-        write(hareToTurtle, &hareDist, sizeof(hareDist));   //say turtle to run
-        while(hareDist < trackLength && turtleDist < trackLength){
-            read(turtleToHare[0], &turtleDist, sizeof(turtleDist));
-            if(haredist < trackLength){
-                read(godToHare, &hareDist, sizeof(hareDist));
-                hareTime ++;
-                hareDist+=3;
+        cout<<"HareProces: "<<getpid()<<endl; 
+        int sleepTime = 0;
+        details hare = {0, 0}, turtle = {0, 0};
+        write(hareToTurtle[1], &hare, sizeof(hare));   //say turtle to run
+        while(hare.dist < trackLength && turtle.dist < trackLength){
+            if(turtle.dist < trackLength)
+                read(turtleToHare[0], &turtle, sizeof(turtle));
+            if(hare.dist < trackLength){
+                read(godToHare[0], &hare.dist, sizeof(hare.dist));
+                if(hare.dist > turtle.dist + 40 && sleepTime == 0){
+                    sleepTime = 60;
+                }
+                hare.time ++;
+                if(sleepTime == 0) hare.dist+=3;
+                else sleepTime--;
             }
+            write(hareToTurtle[1], &hare, sizeof(hare));
         }
 
-        score s;
-        s.hareTime = hareTime;
-        s.turtleTime = turtleTime
-        write(hareToMaster, )
+        result res = {hare.time, turtle.time};
+        write(hareToMaster[1], &res, sizeof(res));
+        return 0;
     }
 
     else if(turtlePid == 0){  //turtle process
         cout<<"turtle process: "<<getpid()<<endl;
-        int turtleDist = 0, time, tempDist;
-        int time = 0;
-        while(turtleDist < trackLength){
-            int nByte = read(hareGod[0], &turtleDist, sizeof(turtleDist));
-            if(nByte < 0){    //pipe is empty
-                cout<<"pipe Empty\n";
-            }
-            turtleDist += 1;
-            time++;
+        details hare = {0, 0}, turtle = {0, 0};
+        while(turtle.dist < trackLength){
+            read(hareToTurtle[0], &hare, sizeof(hare));
+            read(godToHare[0], &turtle.dist, sizeof(turtle.dist));
+            turtle.time++;
+            turtle.dist+=1;
+            write(turtleToHare[1], &turtle, sizeof(turtle));
         }
-
-        write(masterTurtle[1], &time, sizeof(time));
         return 0;
     }
 
     else if(godPid == 0){    //god process
         cout<<"god process: "<<getpid()<<endl;
+        char status;
+        while(true){
+            if((cin>>status));
+        }
     }
 
     else{      //reporter process
--- temp.cpp
+++ temp.cpp
@@ -11,10 +11,27 @@ extern int errno;
 
 int main() 
 { 
-	int arr[2];
-	arr[0] = 1;
-	arr[1] = 2;
-	int &temp = arr[0];
-	temp = 23;
-	cout<<arr[0]<<endl;
+	// int arr[2];
+	// arr[0] = 1;
+	// arr[1] = 2;
+	// int &temp = arr[0];
+	// temp = 23;
+	// cout<<arr[0]<<endl;
+
+	int pid;
+	pid = fork();
+	if(pid > 0){
+		int x;
+		while(cin>>x){
+			cout<<x<<endl;
+			cout<<"god\n";
+			cout<<getpid()<<" "<<pid<<endl;
+		}
+	}
+	else{
+		while(true){
+			cout<<"child\n";
+			sleep(2);
+		}
+	}
 }
