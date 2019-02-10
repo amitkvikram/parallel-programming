99fce78ca62ae6ccb51dd7bfc521d65c86260f9b
amitkvikram <amitkvikram@gmail.com>
2019-02-10 19:11:55 +0530

    file to store temporary codes

--- temp.cpp
+++ temp.cpp
@@ -1,29 +1,55 @@
-#include<bits/stdc++.h>
-#include<pthread.h>
+#include <bits/stdc++.h>
+#include <pthread.h>
+#include <unistd.h>
 
 using namespace std;
 
-void *func1(void *argc){
-    cout<<"Exiting\n";
+typedef struct {
+    int sum;
+    int start;
+    int end;
+} Node;
 
-    return (void*)0;
+int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+void *cal_sum (void *args) {
+
+    Node *node = ((Node *) args);
+    for(int i = node->start; i < node->end; i++) {
+        node->sum += a[i];
+    }
+    cout<<"Node : "<<node->sum;
+    pthread_exit(&(node->sum));
 }
 
-int main(){
-    pthread_t tid;
-    if(pthread_create(&tid, NULL, func1, NULL) != 0){
-        cout<<"Error in creating thread\n";
-    }
-    int x;
-    while(!(cin>>x)){
+int main () {
 
-    }
-    cout<<"Here\n";
+    int p = 4;
+    int n = 10;
+    int steps = n/p;
+    pthread_t sum_threads[p];
+    Node nodes[p];
 
-    if(pthread_detach(tid) != 0){
-        cout<<errno<<endl;
+    for (int i = 0; i < p; i++) {
+        nodes[i].sum = 0;
+        nodes[i].start = i*steps;
+        if(i == p-1) {
+            nodes[i].end = n;
+        } else {
+            nodes[i].end = (i+1)*steps;
+        }
+        pthread_create (&sum_threads[i], NULL, cal_sum, (void *) &nodes[i]);
     }
-    else{
-        cout<<"Detaching Successful\n";
+    Node** sum[p];
+    for (int i = 0; i < p; i++) {
+        pthread_join (sum_threads[i], NULL);
     }
+    Node *x = &nodes[0];
+    *sum[0] = x;
+    int tot_sum = 0;
+    // for(int i = 0; i < p; i++) {
+    //     tot_sum += *(int *)*sum[0];
+    // }
+    cout<<"Total sum = "<<tot_sum<<endl;
+    return 0;
 }
