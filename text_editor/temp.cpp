3c3f4484e153e19a4b0530ad308d90edaca6b7d6
amitkvikram <amitkvikram@gmail.com>
2019-03-18 11:42:32 +0530

    added readme

---
+++ text_editor/temp.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char c;
+    cin>>c;
+    switch (c)
+    {
+        case 'a':
+            cout<<"hello\n";
+            break;
+        case 'b':
+            cout<<"Hii\n";
+            break;
+        default:
+    }
+}
